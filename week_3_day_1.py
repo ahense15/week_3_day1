05e8c66a94e1ee3a1705928b27ad78d6322c7f0b
Hense <ahense@cps.k12.il.us>
2024-09-12 09:17:03 -0500

    Decomstration calculator

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -179,20 +179,40 @@ print(f"the date of birth is {date_of_birth} and the number is {number2}")
 
 ##########################################################################################
 # #addition
+print (2+1)
 # #multiplication
+print(2*2)
 # #division
+print(6/2)
 # #modulo
+print(7%4) #remainder of 7 divided by 4
 # #powers
+print(2**3)
 # #get the max and min of a number
+print("the max of 2 and 3 is" ,max(2,3))
+#max means the highest number
+print("the min of 2 and 3 is" ,min(2,3))
+#min means the lowest number
 # #round a number
+print("round 3.9 is ",round(3.9))
 # # absolute value
+print("the absolute value of -3 is" ,abs(-3))
+#absolute value means the distance from 0 
+# it will always be positive
+
 # # order of operations
+print("2 + 10 * 10 + 3 is", 2 + 10 * 10 +3)
 # #to do more you need to import special math libraries from python
-# #from math import *    
+from math import *    
 # #this goes out and grabs some different math functions we can use
 # #floor method
+print("The floor of 3.7 is" ,floor(3.7))
+print(floor(6.8))
 # #ceil method
+print ("The ceil of 3.7" ,ceil(3.7))
+print(ceil(8.9))
 # #sqrt method
+print(sqrt(64))
 
 
 
@@ -212,15 +232,23 @@ print(f"the date of birth is {date_of_birth} and the number is {number2}")
 # # **Getting Input from users**
 # #how do we get input from users?
 # input("what is your name?")
+name = input("What is your name?")
+print ("hello" + name)
 # # basic math calculator
 # #ask the user for 2 numbers
+num1= int(input("Enter a number."))
+num2= int(input("Enter another number"))
 # # print out a statement where you:
 # # add them together
+print((num1 + num2))
 # #multiply
+print(num1 * num2)
 # # find the max number
+print("The max number of 6 and 7 is",max(6,7))
 # # find the remainder of the numbers
+print ("The remainder of 10/3 is" ,(10%3))
 # #round one number
-
+print("5.6 rounded is", round(5.6))
 
 
 
